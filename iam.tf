4beb5ec10a24944f592e17a64de18dd9ba02468a
Anh Quach <tuananh.quach@moca.vn>
2022-06-19 04:15:27 +0700

    Add role for external-dns

--- iam.tf
+++ iam.tf
@@ -3,3 +3,33 @@ resource "aws_iam_policy" "aws_loadbalancer_controller" {
   name   = "AWSLoadBalancerControllerIAMPolicy"
   policy = file("files/aws-loadbalancer-controller-policy.json")
 }
+
+resource "aws_iam_policy" "external_dns" {
+  name   = "ExternalDNSIAMPolicy"
+  policy = file("files/external-dns-policy.json")
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "ExternalDNSIAMRole"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Effect: "Allow",
+        Principal: {
+          Federated: "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${module.eks.oidc_provider}"
+        },
+        Action: "sts:AssumeRoleWithWebIdentity",
+        Condition: {
+          StringEquals: {
+            "${module.eks.oidc_provider}:aud": "sts.amazonaws.com",
+            "${module.eks.oidc_provider}:sub": "system:serviceaccount:external-dns:external-dns"
+          }
+        }
+      }
+    ]
+  })
+}

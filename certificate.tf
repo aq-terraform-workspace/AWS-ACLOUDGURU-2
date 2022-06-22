7f237f2ef98ca92641966989ef13b53b29379af8
Anh Quach <tuananh.quach@moca.vn>
2022-06-22 23:49:36 +0700

    Add certificate

--- certificate.tf
+++ certificate.tf
@@ -14,7 +14,7 @@ data "aws_route53_zone" "route53" {
 
 resource "aws_route53_record" "cert" {
   for_each = {
-    for dvo in aws_acm_certificate.cert_validation.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type

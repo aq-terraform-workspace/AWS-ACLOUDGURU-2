2facfa9cac024842797e094baefaaa7e750359c3
Anh Quach <tuananh.quach@moca.vn>
2022-06-24 15:36:09 +0700

    Update module reference

--- certificate.tf
+++ certificate.tf
@@ -10,10 +10,6 @@ module "certificate" {
   source  = "git::https://github.com/aq-terraform-modules/terraform-aws-certificate.git?ref=master"
   # version = "1.0.1"
 
-  domain_name = "${var.sub_domain}-${data.aws_caller_identity.current.account_id}.${var.main_domain}"
+  domain_name = module.route53.name
   tags        = module.certificate_label.tags
-
-  depends_on = [
-    module.route53
-  ]
 }

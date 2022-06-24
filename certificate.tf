5ca660098ea879a668f51060fce57068096a8eb2
Anh Quach <tuananh.quach@moca.vn>
2022-06-24 16:52:45 +0700

    Update certificate

--- certificate.tf
+++ certificate.tf
@@ -10,7 +10,8 @@ module "certificate" {
   source  = "git::https://github.com/aq-terraform-modules/terraform-aws-certificate.git?ref=master"
   # version = "1.0.1"
 
-  domain_name = "*.${module.route53.name}"
+  domain_name = module.route53.name
+  sub_domain  = "*"
   tags        = module.certificate_label.tags
 
   depends_on = [module.route53] # Route53 should be created before we create the certificate

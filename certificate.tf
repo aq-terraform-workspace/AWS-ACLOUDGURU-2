acfab670ce30de654650674d7d34ff6cb95b5431
Anh Quach <tuananh.quach@moca.vn>
2022-06-24 15:29:33 +0700

    Update module source git

--- certificate.tf
+++ certificate.tf
@@ -7,7 +7,7 @@ module "certificate_label" {
 
 
 module "certificate" {
-  source  = "git::https://github.com/aq-terraform-modules/terraform-azure-vm-k8s-cluster.git?ref=master"
+  source  = "git::https://github.com/aq-terraform-modules/terraform-aws-certificate.git?ref=master"
   # version = "1.0.1"
 
   domain_name = "${var.sub_domain}-${data.aws_caller_identity.current.account_id}.${var.main_domain}"

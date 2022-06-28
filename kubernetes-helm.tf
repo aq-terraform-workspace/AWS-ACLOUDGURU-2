8f40ccea89c79abe1d2183512a9a68a80d61cfb0
Anh Quach <tuananh.quach@moca.vn>
2022-06-28 14:31:53 +0700

    Test delete

--- kubernetes-helm.tf
+++ kubernetes-helm.tf
@@ -1,12 +1,12 @@
 module "kubernetes_addons" {
   source = "git::https://github.com/aq-terraform-modules/terraform-aws-kubernetes-addons.git?ref=master"
 
-  enable_aws_lb_controller = true
+  enable_aws_lb_controller = false
   aws_lb_controller_context = {
     "clusterName" = module.eks.cluster_id
   }
 
-  enable_ingress_nginx = true
+  enable_ingress_nginx = false
   ingress_nginx_context = {
     "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"          = module.certificate.arn
     "controller.service.internal.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert" = module.certificate.arn
@@ -14,13 +14,13 @@ module "kubernetes_addons" {
 
   enable_cert_manager = false
 
-  enable_external_dns = true
+  enable_external_dns = false
   external_dns_context = {
     "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ExternalDNSIAMRole"
     "domainFilters"                                             = "{${var.sub_domain}-${data.aws_caller_identity.current.account_id}.${var.main_domain}}"
   }
 
-  enable_jenkins = true
+  enable_jenkins = false
   jenkins_context = {
     "controller.jenkinsUrl"       = "jenkins.${var.sub_domain}-${data.aws_caller_identity.current.account_id}.${var.main_domain}"
     "controller.ingress.hostName" = "jenkins.${var.sub_domain}-${data.aws_caller_identity.current.account_id}.${var.main_domain}"

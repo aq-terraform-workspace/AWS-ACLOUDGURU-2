28e86247fd0a4e6d2166926e76a10fcbb54bee3f
Anh Quach <tuananh.qaa@gmail.com>
2022-10-02 16:01:19 +0700

    Revert

--- kubernetes-helm.tf
+++ kubernetes-helm.tf
@@ -1,92 +1,92 @@
-# ###########################################################
-# # EXTERNAL SNAPSHOTTER
-# ###########################################################
-# data "kustomization_build" "ebs_csi" {
-#   path = "files/k8s-custom-manifests"
-# }
-# resource "kustomization_resource" "ebs_csi" {
-#   for_each = data.kustomization_build.ebs_csi.ids
+###########################################################
+# EXTERNAL SNAPSHOTTER
+###########################################################
+data "kustomization_build" "ebs_csi" {
+  path = "files/k8s-custom-manifests"
+}
+resource "kustomization_resource" "ebs_csi" {
+  for_each = data.kustomization_build.ebs_csi.ids
 
-#   manifest = data.kustomization_build.ebs_csi.manifests[each.value]
-# }
+  manifest = data.kustomization_build.ebs_csi.manifests[each.value]
+}
 
 
-# module "kubernetes_addons" {
-#   source = "git::https://github.com/aq-terraform-modules/terraform-aws-kubernetes-addons.git?ref=master"
+module "kubernetes_addons" {
+  source = "git::https://github.com/aq-terraform-modules/terraform-aws-kubernetes-addons.git?ref=master"
 
-#   # Basic variables
-#   base_label_context = module.base_label.context
-#   oidc_provider = module.eks.oidc_provider
+  # Basic variables
+  base_label_context = module.base_label.context
+  oidc_provider = module.eks.oidc_provider
 
-#   # Snapscheduler
-#   enable_snapscheduler = false
+  # Snapscheduler
+  enable_snapscheduler = false
 
-#   # EFS CSI Driver
-#   enable_efs_csi_driver  = false
-#   efs_network_properties = {
-#     vpc_id = module.base_network.vpc_id
-#     subnets = module.base_network.private_subnets
-#     subnets_cidr_block = module.base_network.private_subnets_cidr_blocks
-#   }
+  # EFS CSI Driver
+  enable_efs_csi_driver  = false
+  efs_network_properties = {
+    vpc_id = module.base_network.vpc_id
+    subnets = module.base_network.private_subnets
+    subnets_cidr_block = module.base_network.private_subnets_cidr_blocks
+  }
 
-#   # LB Controller
-#   enable_aws_lb_controller = false
-#   aws_lb_controller_context = {
-#     "clusterName" = module.eks.cluster_id
-#   }
+  # LB Controller
+  enable_aws_lb_controller = false
+  aws_lb_controller_context = {
+    "clusterName" = module.eks.cluster_id
+  }
 
-#   # Prometheus
-#   enable_prometheus = false
-#   prometheus_context = {
-#     "grafana.ingress.hosts" = "{grafana.${var.sub_domain}-${local.account_id}.${var.main_domain}}"
-#     "prometheus.ingress.hosts" = "{prometheus.${var.sub_domain}-${local.account_id}.${var.main_domain}}"
-#   }
+  # Prometheus
+  enable_prometheus = false
+  prometheus_context = {
+    "grafana.ingress.hosts" = "{grafana.${var.sub_domain}-${local.account_id}.${var.main_domain}}"
+    "prometheus.ingress.hosts" = "{prometheus.${var.sub_domain}-${local.account_id}.${var.main_domain}}"
+  }
 
-#   # Ingress Nginx
-#   enable_ingress_nginx = false
-#   ingress_nginx_context = {
-#     "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"          = module.certificate.arn
-#     "controller.service.internal.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert" = module.certificate.arn
-#   }
+  # Ingress Nginx
+  enable_ingress_nginx = false
+  ingress_nginx_context = {
+    "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"          = module.certificate.arn
+    "controller.service.internal.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert" = module.certificate.arn
+  }
 
-#   # External DNS
-#   enable_external_dns = false
-#   external_dns_context = {
-#     "domainFilters" = "{${var.sub_domain}-${local.account_id}.${var.main_domain}}"
-#   }
+  # External DNS
+  enable_external_dns = false
+  external_dns_context = {
+    "domainFilters" = "{${var.sub_domain}-${local.account_id}.${var.main_domain}}"
+  }
 
-#   # Jenkins
-#   enable_jenkins = false
-#   jenkins_chart_version = "4.1.14"
-#   jenkins_context = {
-#     "controller.jenkinsUrl"       = "jenkins.${var.sub_domain}-${local.account_id}.${var.main_domain}"
-#     "controller.ingress.hostName" = "jenkins.${var.sub_domain}-${local.account_id}.${var.main_domain}"
-#   }
+  # Jenkins
+  enable_jenkins = false
+  jenkins_chart_version = "4.1.14"
+  jenkins_context = {
+    "controller.jenkinsUrl"       = "jenkins.${var.sub_domain}-${local.account_id}.${var.main_domain}"
+    "controller.ingress.hostName" = "jenkins.${var.sub_domain}-${local.account_id}.${var.main_domain}"
+  }
 
-#   # Secret CSI Driver
-#   enable_secret_csi = true
+  # Secret CSI Driver
+  enable_secret_csi = true
 
-#   # Vault
-#   enable_vault = true
-#   vault_context = {
-#     "server.ingress.hosts[0].host" = "vault.${var.sub_domain}-${local.account_id}.${var.main_domain}"
-#   }
+  # Vault
+  enable_vault = true
+  vault_context = {
+    "server.ingress.hosts[0].host" = "vault.${var.sub_domain}-${local.account_id}.${var.main_domain}"
+  }
 
-#   # Cert Manager
-#   enable_cert_manager = false
+  # Cert Manager
+  enable_cert_manager = false
 
-#   # Velero
-#   enable_velero = false
+  # Velero
+  enable_velero = false
 
-#   # Keda
-#   enable_keda = true
+  # Keda
+  enable_keda = true
 
-#   # Linkerd
-#   enable_linkerd = true
+  # Linkerd
+  enable_linkerd = true
 
-#   depends_on = [
-#     module.eks,
-#     module.certificate.arn,
-#     kustomization_resource.ebs_csi
-#   ]
-# }
+  depends_on = [
+    module.eks,
+    module.certificate.arn,
+    kustomization_resource.ebs_csi
+  ]
+}
--- locals.tf
+++ locals.tf
@@ -8,35 +8,35 @@ locals {
   # Hack for Kustomization
   ###########################################################
   # non-default context name to protect from using wrong kubeconfig
-  # kubeconfig_context = "_terraform-kustomization-${module.eks.cluster_id}_"
+  kubeconfig_context = "_terraform-kustomization-${module.eks.cluster_id}_"
 
-  # kubeconfig = {
-  #   apiVersion = "v1"
-  #   clusters = [
-  #     {
-  #       name = local.kubeconfig_context
-  #       cluster = {
-  #         certificate-authority-data = module.eks.cluster_certificate_authority_data
-  #         server                     = module.eks.cluster_endpoint
-  #       }
-  #     }
-  #   ]
-  #   users = [
-  #     {
-  #       name = local.kubeconfig_context
-  #       user = {
-  #         token = data.aws_eks_cluster_auth.main.token
-  #       }
-  #     }
-  #   ]
-  #   contexts = [
-  #     {
-  #       name = local.kubeconfig_context
-  #       context = {
-  #         cluster = local.kubeconfig_context
-  #         user    = local.kubeconfig_context
-  #       }
-  #     }
-  #   ]
-  # }
+  kubeconfig = {
+    apiVersion = "v1"
+    clusters = [
+      {
+        name = local.kubeconfig_context
+        cluster = {
+          certificate-authority-data = module.eks.cluster_certificate_authority_data
+          server                     = module.eks.cluster_endpoint
+        }
+      }
+    ]
+    users = [
+      {
+        name = local.kubeconfig_context
+        user = {
+          token = data.aws_eks_cluster_auth.main.token
+        }
+      }
+    ]
+    contexts = [
+      {
+        name = local.kubeconfig_context
+        context = {
+          cluster = local.kubeconfig_context
+          user    = local.kubeconfig_context
+        }
+      }
+    ]
+  }
 }

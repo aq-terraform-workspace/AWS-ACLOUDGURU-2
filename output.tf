b1a07cbd6016971cbe2c53610b43cf93642a1607
Anh Quach <tuananh.qaa@gmail.com>
2022-10-02 16:59:51 +0700

    Add output for cluster ID

--- output.tf
+++ output.tf
@@ -1,18 +1,27 @@
+# Bastion Host
 output "bastion_public_ip" {
   description = "Public IP of the bastion VM"
   value       = try(aws_eip.bastion.public_ip, "")
 }
 
+# EKS
 output "cluster_endpoint" {
   description = "The endpoint for your EKS Kubernetes API."
   value       = try(module.eks.cluster_endpoint, "")
 }
+
+output "cluster_id" {
+  description = "The endpoint for your EKS Kubernetes API."
+  value       = try(module.eks.cluster_id, "")
+}
+
 
 output "aws_auth_configmap_yaml" {
   description = "Formatted yaml output for base aws-auth configmap containing roles used in cluster node groups/fargate profiles"
   value       = try(module.eks.aws_auth_configmap_yaml, "")
 }
 
+# Route53
 output "route53_zone_name" {
   description = "Domain name of the Route53 zone"
   value       = try(module.route53.name, "")

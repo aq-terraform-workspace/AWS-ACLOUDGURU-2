27b8a5d1cf2eec86ebd5ecdda4281384db375f24
Anh Quach <tuananh.qaa@gmail.com>
2022-10-02 15:54:58 +0700

    Revert

--- route53.tf
+++ route53.tf
@@ -1,8 +1,8 @@
-# module "route53" {
-#   source      = "git::https://github.com/aq-terraform-modules/terraform-aws-route53.git?ref=dev"
-#   main_domain = var.main_domain
-#   sub_domain  = "${var.sub_domain}-${local.account_id}"
-# }
+module "route53" {
+  source      = "git::https://github.com/aq-terraform-modules/terraform-aws-route53.git?ref=dev"
+  main_domain = var.main_domain
+  sub_domain  = "${var.sub_domain}-${local.account_id}"
+}
 
 resource "null_resource" "health_check" {
 
@@ -12,15 +12,15 @@ resource "null_resource" "health_check" {
   }
 }
 
-# module "cloudflare_records" {
-#   # providers = {
-#   #   cloudflare = cloudflare
-#   # }
+module "cloudflare_records" {
+  # providers = {
+  #   cloudflare = cloudflare
+  # }
 
-#   source       = "git::https://github.com/aq-terraform-modules/terraform-cloudflare-general.git?ref=dev"
-#   main_domain  = var.main_domain
-#   sub_domain   = length(var.sub_domain) > 0 ? "${var.sub_domain}-${local.account_id}" : local.account_id
-#   name_servers = module.route53.name_servers
+  source       = "git::https://github.com/aq-terraform-modules/terraform-cloudflare-general.git?ref=dev"
+  main_domain  = var.main_domain
+  sub_domain   = length(var.sub_domain) > 0 ? "${var.sub_domain}-${local.account_id}" : local.account_id
+  name_servers = module.route53.name_servers
 
-#   depends_on = [module.route53] # Route53 should be created before we create additional NS records on cloudflare
-# }
+  depends_on = [module.route53] # Route53 should be created before we create additional NS records on cloudflare
+}

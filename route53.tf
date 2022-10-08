15229e12a375c92ab6d19bf5e498192ea5d555f8
Anh Quach <tuananh.qaa@gmail.com>
2022-10-08 23:28:28 +0700

    Disable CF purge

--- route53.tf
+++ route53.tf
@@ -5,19 +5,19 @@ module "route53" {
   # sub_domain  = "${var.sub_domain}-${local.account_id}"
 }
 
-resource "null_resource" "cloudflare_purge" {
-  triggers = {
-    "script_change" = file("${path.root}/scripts/cloudinit.yaml")
-  }
+# resource "null_resource" "cloudflare_purge" {
+#   triggers = {
+#     "script_change" = file("${path.root}/scripts/cloudinit.yaml")
+#   }
 
-  provisioner "local-exec" {
-    working_dir = "${path.root}/scripts" # assuming it's this directory
-    command     = <<-EOT
-      chmod +x cloudflare_purge_records.sh
-      ./cloudflare_purge_records.sh
-    EOT
-  }
-}
+#   provisioner "local-exec" {
+#     working_dir = "${path.root}/scripts" # assuming it's this directory
+#     command     = <<-EOT
+#       chmod +x cloudflare_purge_records.sh
+#       ./cloudflare_purge_records.sh
+#     EOT
+#   }
+# }
 
 module "cloudflare_records" {
   source       = "git@github.com:aq-terraform-modules/terraform-cloudflare-general.git?ref=dev"
@@ -27,7 +27,7 @@ module "cloudflare_records" {
   # sub_domain   = length(var.sub_domain) > 0 ? "${var.sub_domain}-${local.account_id}" : local.account_id
 
   depends_on = [
-    null_resource.cloudflare_purge, # Old records should be purged before creating new records
+    # null_resource.cloudflare_purge, # Old records should be purged before creating new records
     module.route53,                 # Route53 should be created before we create additional NS records on cloudflare
   ]
 }
